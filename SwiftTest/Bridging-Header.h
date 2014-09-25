fbbe1c2d38fed77f4b76ccccd45b17ad9614a917
Roland Gröpmair <roland@intercom.io>
2014-09-25 12:14:26 +0100

    adding Intercom via cocoapods

---
+++ SwiftTest/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  SwiftTest
+//
+//  Created by Roland Gröpmair on 25/09/2014.
+//  Copyright (c) 2014 Intercom. All rights reserved.
+//
+
+#ifndef SwiftTest_Bridging_Header_h
+#define SwiftTest_Bridging_Header_h
+
+#import <Intercom/Intercom.h> 
+
+#endif
